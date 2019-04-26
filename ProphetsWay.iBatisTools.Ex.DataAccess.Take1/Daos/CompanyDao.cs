db2cebb56df7f5a07e2f65280de0b5a336c724ab
prophet <prophetmanx@gmail.com>
2019-04-26 17:22:01 -0400

    created a base paged dao

--- ProphetsWay.iBatisTools.Ex.DataAccess.Take1/Daos/CompanyDao.cs
+++ ProphetsWay.iBatisTools.Ex.DataAccess.Take1/Daos/CompanyDao.cs
@@ -5,30 +5,14 @@ using System.Collections.Generic;
 
 namespace ProphetsWay.iBatisTools.Ex.DataAccess.Take1.Daos
 {
-    internal class CompanyDao : BaseDao<Company>, ICompanyDao
+    internal class CompanyDao : BasePagedDao<Company>, ICompanyDao
     {
         public CompanyDao(ISqlMapper mapper) : base(mapper) { }
-
-        public int GetCount(Company item)
-        {
-            const string stmtId = "Company.GetCount";
-            return Mapper.QueryForObject<int>(stmtId, null);
-        }
 
         public Company GetCustomCompanyFunction(int id)
         {
             const string stmtId = "Company.CustomFunction";
             return Mapper.QueryForObject<Company>(stmtId, id);
-        }
-
-        public IList<Company> GetPaged(Company item, int skip, int take)
-        {
-            const string stmtId = "Company.GetPaged";
-            var param = new Dictionary<string, object>();
-            param.Add("Offset", skip);
-            param.Add("PageSize", take);
-
-            return Mapper.QueryForList<Company>(stmtId, param);
         }
     }
 }
