77d3d1da3842344de7b6ed79b85f037af343c246
prophet <prophetmanx@gmail.com>
2019-04-26 17:09:59 -0400

    got the database project created

---
+++ ProphetsWay.iBatisTools.Ex.Database/Tables/Jobs.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Jobs] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [Name]      VARCHAR (MAX) NULL,
+    [Something] VARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Jobs] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ ProphetsWay.iBatisTools.Ex.Database/Tables/Users.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [Name]      VARCHAR (MAX) NULL,
+    [Whatever]  VARCHAR (MAX) NULL,
+    [CompanyId] INT           NULL,
+    [JobId]     INT           NULL,
+    CONSTRAINT [FK_Users_Companies] FOREIGN KEY ([CompanyId]) REFERENCES [dbo].[Companies] ([Id]),
+    CONSTRAINT [FK_Users_Jobs] FOREIGN KEY ([JobId]) REFERENCES [dbo].[Jobs] ([Id]), 
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id])
+);
+
