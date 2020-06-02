d0cb789bf61df05c66b4deb2036a8330cf4553a3
G. Gordon Nasseri <ProphetManX@gmail.com>
2020-06-02 17:47:08 -0400

    removed the old projects from source and references from the solution

--- ProphetsWay.iBatisTools.Ex.Database/Tables/Companies.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Companies] (
-    [Id]    INT           IDENTITY (1, 1) NOT NULL,
-    [Name]  VARCHAR (MAX) NULL,
-    [Other] VARCHAR (MAX) NULL,
-    CONSTRAINT [PK_Companies] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
